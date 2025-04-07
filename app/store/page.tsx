8f4a617c7e62b3dc7cf2d10a15b0ffaa56fe2fba
ammarrqureshi <mamqureshi786@gmail.com>
2025-04-08 00:12:04 +0500

    basics set

---
+++ app/store/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import { Button} from "@/components/ui/button"
+export default function  Store() {
+  return (
+    <div>
+Products List here
+      <Button> Add to Cart</Button>
+    </div>
+  )
+}
+
