80a7a9ae10e0097eb8eab53d099fbba5a3c08704
sadiaKamal <sadiakamal2468@gmail.com>
2025-06-23 23:03:39 +0500

    updated again

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,8 @@ export default function Home() {
   const products = [
     { id: 1, name: 'T-Shirt', price: 19.99, image: '/t-shirt.jpg' },
     { id: 2, name: 'Jeans', price: 49.99, image: '/jeans.jpg' },
+        { id: 2, name: 'Jeans', price: 49.99, image: '/jeans.jpg' },
+
     
   ];
 
